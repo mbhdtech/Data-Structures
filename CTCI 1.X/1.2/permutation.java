e24bf5ee3e98f6107600cc2e689207bd649b1a71
Matthew Brandon <matthewbrandon@Matthews-MBP.fios-router.home>
2017-08-07 19:21:59 -0400

    Added Instruction and Java file for solution
    Actually code needs to be added later

---
+++ CTCI 1.X/1.2/permutation.java
@@ -0,0 +1,15 @@
+
+public class permutation {
+    public static void main(String[]args){
+        String word1 = "Please";
+        String word2 = "Nope";
+
+            boolean answer = permutation(word1, word2);
+            System.out.print(answer);
+        }
+
+        public static boolean permutation(String str1, String str2){
+        }
+}
+
+
