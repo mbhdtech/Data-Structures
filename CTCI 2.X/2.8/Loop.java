cd604bdca1789feb28abd6ebb5c3af43cd931699
Matthew Brandon <matthewbrandon@Matthews-MacBook-Pro.local>
2017-08-24 17:46:20 -0400

    Minor Mistake with 1.3

---
+++ CTCI 2.X/2.8/Loop.java
@@ -0,0 +1,37 @@
+
+public class URLify {
+    public static void main(String[]args){
+        String word1 = "foobar";
+        String word2 = "rabofo";
+
+            boolean answer = permutation(word1, word2);
+            System.out.print(answer);
+        }
+
+        public static boolean permutation(String str1, String str2){
+            if(str1.length() != str2.length()){
+                 return false;
+            }
+
+            else{
+                char[] word1 = str1.toCharArray();
+                char[] word2 = str2.toCharArray();
+
+                java.util.Arrays.sort(word1);
+                java.util.Arrays.sort(word2);
+
+                str1 = new String(word1);
+                str2 = new String(word2);
+
+                    if(str1.equals(str2)){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+        }
+
+    }
+}
+
+
