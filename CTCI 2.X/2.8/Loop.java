d52f9143113a104e6a97fad3278430f705c75b20
Matthew Brandon <matthewbrandon@Matthews-MacBook-Pro.local>
2017-08-24 17:50:10 -0400

    Solution and question for 2.8

--- CTCI 2.X/2.8/Loop.java
+++ CTCI 2.X/2.8/Loop.java
@@ -1,36 +1,59 @@
 
-public class URLify {
-    public static void main(String[]args){
-        String word1 = "foobar";
-        String word2 = "rabofo";
+import java.util.*;
+public class Loop {
+    public static void main(String[] args) {
 
-            boolean answer = permutation(word1, word2);
-            System.out.print(answer);
-        }
+        //Creating random nodes
+        Node head = new Node(4);
+        Node node1 = new Node(6);
+        Node node2 = new Node(7);
+        Node node3 = new Node(8);
+        Node node4 = new Node(6);
+        Node node5 = new Node(12);
+        Node node6 = new Node(9);
 
-        public static boolean permutation(String str1, String str2){
-            if(str1.length() != str2.length()){
-                 return false;
+        //Creating a circular linked list
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node3;
+
+
+        Node answer = circle(head);
+        System.out.println(answer.data);
+    }
+
+    public static Node circle(Node head) {
+        HashSet<Node> set = new HashSet<Node>();
+        Node curr = head;
+
+        while (curr != null) {
+            if (set.contains(curr)) {
+                return curr;
             }
-
-            else{
-                char[] word1 = str1.toCharArray();
-                char[] word2 = str2.toCharArray();
-
-                java.util.Arrays.sort(word1);
-                java.util.Arrays.sort(word2);
-
-                str1 = new String(word1);
-                str2 = new String(word2);
-
-                    if(str1.equals(str2)){
-                        return true;
-                    }
-                    else{
-                        return false;
-                    }
+            else {
+                set.add(curr);
+            }
+            curr = curr.next;
         }
 
+        System.out.println("The list isn't circular");
+        return curr;
+    }
+
+}
+
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        Node next = null;
     }
 }
 
