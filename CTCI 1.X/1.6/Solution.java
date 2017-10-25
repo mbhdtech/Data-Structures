341ce06ec1677dbf8359c790c94e067b65d8a42b
Matthew Brandon <matthewbrandon@Matthews-MacBook-Pro.local>
2017-10-24 21:54:52 -0400

    Added 1.6 from CTCI

---
+++ CTCI 1.X/1.6/Solution.java
@@ -0,0 +1,42 @@
+
+public class Solution{
+    public static void main(String[] args){
+
+        System.out.println(strCompressor("aabcccccbc"));
+
+    }
+
+
+    public static String strCompressor(String str){
+
+        int counter = 0;
+        char character = str.charAt(counter);
+        StringBuilder comp = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++){
+
+
+            if(character != str.charAt(i)){
+                comp.append(character);
+                comp.append(String.valueOf(counter));
+                counter = 1;
+                character = str.charAt(i);
+            }
+
+            else{
+                counter++;
+            }
+
+            if(i == str.length()-1){
+                comp.append(character);
+                comp.append(counter);
+            }
+
+        }
+
+        return comp.toString();
+    }
+
+
+
+}
